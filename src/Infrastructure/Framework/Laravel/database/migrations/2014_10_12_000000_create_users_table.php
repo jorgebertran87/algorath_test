109fa6ee61c6317c52cabfad9db377365f41bafe
jorgebertran87 <jorgebertran87@gmail.com>
2018-11-09 14:31:52 +0100

    Add migrations

--- src/Infrastructure/Framework/Laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/Infrastructure/Framework/Laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,30 +6,17 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('id', 10);
+            $table->string('name', 50);
             $table->timestamps();
+            $table->primary('id');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
